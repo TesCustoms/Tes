2daa8acf9b97f7c1f90ff4906fcf7cf387b28ba9
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2019-06-16 11:17:50 -0700

    The fun begins

--- TesMuffler/LaserPING.py
+++ TesMuffler/LaserPING.py
@@ -1,10 +1,26 @@
+# Used to control GPIO pins and control motors, servos, and relays 
 import gpiozero
 
+# Used to control the space-time fabric :)
+import time      
+
+# Useful CONSTANSTS 
 DEBUG_STATEMENTS_ON = True
+HIGH = 1
+LOW = 0
+
+# LASER CONSTANTS 
+SPEED_OF_LIGHT = 299,792,458 # Units are m/s
 
 class LaserPING:
 
-def getDistance():
+def getDistance(units):
+    if(units == CM)):
+    elif(units == IN):
+    else:
+        print(“INCORREC units PARAMETER PASSED TO getDistance() FUNCTION”)
+
+    return distanceCM
 
 def adjustSpeedOfLigth():
 
@@ -12,4 +28,6 @@ def debugPrint():
 
 
 if __name__ == “__main__”:
-print(“Start Unit
+    print(“START LaserPING.py UNIT TEST”)
+
+    print(“END LaserPING.py  UNIT TEST”)
