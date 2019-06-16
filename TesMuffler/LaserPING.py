50fd4a27fae1f675b3287099be21030805c747ad
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2019-06-16 11:06:34 -0700

    Update LaserPING.py

--- TesMuffler/LaserPING.py
+++ TesMuffler/LaserPING.py
@@ -2,5 +2,14 @@ import gpiozero
 
 DEBUG_STATEMENTS_ON = True
 
-if __name__ == “__main__”:
+class LaserPING:
 
+def getDistance():
+
+def adjustSpeedOfLigth():
+
+def debugPrint():
+
+
+if __name__ == “__main__”:
+print(“Start Unit
