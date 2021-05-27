124960eb3850d402e398c34a10d99587b52114ac
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2021-05-27 15:04:26 +0000

    WIP: Making installing TesMuffler A LOT easier

--- TesMuffler/install-2.py
+++ TesMuffler/install-2.py
@@ -1,36 +1,74 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 __author__ =  "Blaze Sanders"
 __email__ =   "blaze.d.a.sanders@gmail.com"
 __company__ = "Humanity"
 __status__ =  "Development"
-__date__ =    "Late Updated: 2021-04-27"
-__doc__ =     "Install script to setup TesMuffler dev enviroment"
+__date__ =    "Late Updated: 2021-05-27"
+__doc__ =     "Setup a TesMuffler dev enviroment using VirtualEnv"
 """
 
-CONFIG = "Pi3B+" # or "UnbuntuOnWindows" of "UbuntuMate" or "Alpine"
+
+# Hardware that code can run on CONSTANTS
+PI_4    = "Raspberry Pi 4"
+RP_2040 = "Arduino Microcontroller"
+PI_PICO = "Raspbery Pi Microntroller"
+
+
+# Allow for easy command-line install of all TesMuffler dependencies
+# https://docs.python.org/3/library/argparse.html
+import argparse
+
+# Allow install to pause, allowing programmer to read terminal output
+# https://docs.python.org/3/library/time.html
+import time
 
 # Allow BASH commands to be run inside python code like this file
+# https://docs.python.org/3/library/subprocess.html
 import subprocess
 from subprocess import Popen, PIPE
 from subprocess import check_call
 
+
 if __name__ == "__main__":
-	check_call("clear",shell=True)  # Clear terminal
+
+	parser = argparse.ArgumentParser(prog = "TesMuffler install script", description = __doc__, add_help=True)
+	parser.add_argument("-d", "--device", type=str, default= PI_4, choices=[PI_4, RP2040, PI_PICO], help="Select the hardware code is running on">
+	#TODO Add to TesMufflerDriver.py - parser.add_argument("-t", "--trace", type=int, default=0, help="Program trace level.")
+	args = parser.parse_args()
+
+	hardware = args.device
+
+	check_call("clear",shell=True)  	# Clear terminal
+	print("Starting TesMuffler install")
+	time.sleep(2)				# Pause 2 seconds
 
 	# Check and update your system
 	check_call("sudo apt update", shell=True)
 	check_call("sudo apt upgrade", shell=True)
+
+	# Install PIP3 to stay away from all things Python 2!
+	check_call("sudo apt install python-pip3", shell=True)
+
+	# Start autoconfiguring a virtual enviroment like a good programmer should :)
+	# https://www.geeksforgeeks.org/python-virtual-environment/
+	check_call("pip3 install virtualenv", shell=True)
+	check_call("virtualenv --version", shell=True)
+	time.sleep(2)
+	check_call("virtualenv TesMufflerDevEnv", shell=True)
+	# Specifiy Python 3 interpreter to stay away from all things Python 2!
+	check_call("virtualenv -p /usr/bin/python3 virtualenv_name", shell=True)
 	
-	python-pip3
-	pipenv
-	https://pypi.org/project/energyusage/
-	
-	https://github.com/scottrogowski/mongita
-	
-	https://www.builtinafrica.io/blog-post/vuyisile-ndlovu-pypi
-	
-	https://docs.python.org/3/library/pathlib.html
+
+check_call("", shell=True)
+
+source virtualenv_name/bin/activate
+	#TODO COOL EXTRAS
+	#pipenv
+	#https://pypi.org/project/energyusage/
+	#https://github.com/scottrogowski/mongita
+	#https://www.builtinafrica.io/blog-post/vuyisile-ndlovu-pypi
+	#https://docs.python.org/3/library/pathlib.html
 
 	# Allow other computers to SSH into Pi running this code
 	# Needed since SSH is not always installed on Pi distros
