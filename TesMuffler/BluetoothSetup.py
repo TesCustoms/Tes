ea425d66c93f76aa68ecacf01a496602b9754868
OpenSourceIronman <blaze.d.a.sanders@gmail.com>
2021-07-31 10:59:17 -0700

    fix 95% of the flake8 warnings

--- TesMuffler/BluetoothSetup.py
+++ TesMuffler/BluetoothSetup.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
+
 """
 __author__  = "Blaze Sanders"
 __email__   = "blaze.d.a.sanders@gmail.com"
 __status__  = "Development"
 __date__    = "Late Updated: 2021-07-18"
-__doc__     = "Class to automate bluetooth pairing of hardware & mobile app to any Tesla vehicle"
+__doc__     = "Class to automate bluetooth pairing of hardware & mobile app to any Tesla vehicle"  # NOQA: E501
 """
 
-# Allow Pi, Linux MacOS, and Windows CPUs to access host machine Bluetooth resources
+# Allow Linux,MacOS, ESP-32 CPUs to access host machine Bluetooth resources
 # https://pypi.org/project/PyBluez/
 import bluetooth
 
 # Create unique random ID for each vehicle for user privacy
-#https://docs.python.org/3/library/uuid.html
+# https://docs.python.org/3/library/uuid.html
 import uuid
 
 # Allow program to extract filename of the current file
@@ -20,111 +21,128 @@ import uuid
 import os
 
 try:
-    # Generate .txt data logging and custom terminal debugging output
-    from Debug import *
-
+    # Generate .txt debug logs and custom terminal debugging print statement
+    from Debug import Dprint, Lprint
     #TODO
 
 except ImportError:
-    print("Debug didn't import correctly, please verify that Debug.py is in directory of BluetoothSetup.py")
+    print("Debug didn't import correctly, "
+          "please verify that Debug.py is in directory of BluetoothSetup.py")
     #TODO
 
 
 class BluetoothSetup:
 
-	TODO = -1
+    TODO = -1
 
-	# Model model name CONTSTANTS
-	MODEL_S = 'S'
-	MODEL_3 = '3'
-	MODEL_X = 'X'
-	MODEL_Y = 'Y'
-	CYBER_TRUCK = 'C'
-	ATV = 'A'
-	ROADSTER_V2 = 'R'
-	SEMI_TRUCK = 'S'
-	ALL = 'S3XY CARS'
+    # Model model name CONTSTANTS
+    MODEL_S = 'S'
+    MODEL_3 = '3'
+    MODEL_X = 'X'
+    MODEL_Y = 'Y'
+    CYBER_TRUCK = 'C'
+    ATV = 'A'
+    ROADSTER_V2 = 'R'
+    SEMI_TRUCK = 'S'
+    ALL = 'S3XY CARS'
 
-	# Bluetooth clas type CONSTANTS
-	TELSA_BLUETOOTH_CLASS = TODO
-	CELLPHONE_BLUETOOTH_CLASS = TODO
-	HEADPHONE_BLUETOOTH_CLASS = TODO
+    # Bluetooth class type CONSTANTS
+    TELSA_BLUETOOTH_CLASS = TODO
+    CELLPHONE_BLUETOOTH_CLASS = TODO
+    HEADPHONE_BLUETOOTH_CLASS = TODO
 
-	# Debugging CONSTANTS
-	DEBUG_STATEMENTS_ON = True
+    # Debugging CONSTANTS
+    DEBUG_STATEMENTS_ON = True
 
+    def __init__(self, model=ALL):
+        """Construcor to find & connnect to the strongest Tesla Bluetooth signal
 
-	def __init__(self, model=ALL):
-		"""
-		Args:
-			model [CHAR] Optional filter to TODO
-		"""
-		thisCodesFilename = os.path.basename(__file__)
-		self.DebugObject = Debug(BlueboothSetup.DEBUG_STATEMENTS_ON, thisCodesFilename)
+        Args:
+            model (char, optional): Selection filter to limit the car models
+            that bluetooth will atttempt to pair and connect to
 
-		# https://www.w3schools.com/python/python_lists_methods.asp
-		id = uuid4()
-		self.deviceIdList = [id]
+        Returns:
+            Newly created BluetoothSetup() object
+        """
 
-		bluetoothDevices = findDevices(model)
-		numOfDevices = len(bluetoothDevices)
-		for i in range(0, numOfDevices):
-			newId = uuid4()
-			self.deviceIdList.append(newId)
+        thisCodesFilename = os.path.basename(__file__)
+        self.DebugObject = Debug(BlueboothSetup.DEBUG_STATEMENTS_ON,
+                                 thisCodesFilename)
 
-		#Strongest bluetooth signal afte search for 42 seconds
-		for j in range(0,numOfDevices):
-			if(bluetoothDevices.lookup_name(TODO, 42) == "Tesla"):
-				print("TODO")
+        # https://www.w3schools.com/python/python_lists_methods.asp
+        id = uuid.uuid4()
+        self.deviceIdList = [id]
 
-			if(bluetoothDevices[listIndex][tupleIndex] == BluetoothSetup.TESLA_BLUETOOTH_CLASS):
-				# pairWithVehicle()
-				self.connectedCar = TODO #connectToVehicle()
+        bluetoothDevices = findDevices(model)
+        numOfDevices = len(bluetoothDevices)
+        for i in range(0, numOfDevices):
+            newId = uuid4()
+            self.deviceIdList.append(newId)
 
+        # Connect to the strongest bluetooth signal after 42 second search
+        for j in range(0, numOfDevices):
+            if(bluetoothDevices.lookup_name(TODO, 42) == "Tesla"):
+                print("TODO")
 
+        for listIndex in range(0, numOfDevices):
+            for tupleIndex in range(0, 3):
+                if(bluetoothDevices[listIndex][tupleIndex] == BluetoothSetup.TESLA_BLUETOOTH_CLASS):  # NOQA: E501
+                    # pairWithVehicle()
+                    self.connectedCar = TODO #connectToVehicle(bluetoothDevice[listIndex][tupleIndex])
 
-	def unitTest():
-		TestObject1 = BluetoothSetup('C')
+        def unitTest():
+            """Test connecting ESP-32 to a Tesla Cybertruck
 
-		TestObject1.disconnectFromVehicle()
+            Args:
+                NONE
 
+            Returns:
+                NOTHING
+            """
 
-	def findDevices(self):
-		"""Search for ANY nearby device with a strong Bluetooth signal
+            TestObject1 = BluetoothSetup('C')
+            TestObject1.disconnectFromVehicle()
 
-		Args:
-			NONE
+        def findDevices(self):
+            """Search for ANY nearby device with a strong Bluetooth signal
 
-		Returns:
-			Tuple (address, human reable name, bluetooth class type TODO) of bluetooth devices within range
-		"""
-		nearbyDevices = bluetooth.discover_devices(lookup_names=True, lookup_class=True)
+            Args:
+                NONE
 
-		return nearbyDevices
+            Returns:
+                A Tuple (address, human reable name, bluetooth class type TODO)
+                of all bluetooth devices within range #TODO(~100 meters)
+            """
 
+            nearbyDevices = bluetooth.discover_devices(lookup_names=True,
+                                                       lookup_class=True)
 
-	def pairWithVehicle():
-		"""TODO
-		"""
-		print("TODO")
+            return nearbyDevices
 
-	def connectToVehicle():
-		"""TODO
-		"""
-		print("TODO")
+       def pairWithVehicle():
+           """TODO
+	   """
 
+           print("TODO")
 
-	def disconnectFromVehicle():
-		"""TODO
-		"""
-		print("TODO")
+       def connectToVehicle():
+           """TODO
+           """
+
+           print("TODO")
+
+       def disconnectFromVehicle():
+           """TODO
+           """
+
+           print("TODO")
 
 
 if __name__ == "__main__":
 
 	try:
 		Bluetoothsetup.unitTest()
-	except AssertionError as error: #NameError:
+	except AssertionError as error: #TODO Switch to NameError?
 		print("BluetoothSetup Unit Test failed :(")
 
 	print("END of BluetoothSetup.py MAIN method")
