b58f287ef3f898d2235c8e2baebd46abc76d36aa
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2022-09-26 14:49:33 -0700

    WIP: Interface specific CAN Bus details and documentation
    Reordered vehicle make and model CONSTANTS, and edited pedal CONSTANTS
    Updated function calls inside readXXX() functions
    Added ISO-TP URL and pip3 install to install.py

--- TesMuffler/GlobalConstant.py
+++ TesMuffler/GlobalConstant.py
@@ -9,12 +9,12 @@ __doc__     = "Useful global constants used across all TesMuffler code base"
 
 # Vehicle make name CONTSTANTS - Yes these are sorted best to worst :)
 TESLA = 0
-RIVIAN = 1
-APTERA = 2
-LUCID = 3
-FORD = 4
+APTERA = 1
+RIVIAN = 2
+FORD = 3
+KIA = 4
 
-# Vehicle model name CONTSTANTS
+# Vehicle model name CONTSTANTS for vehicle makes above
 MODEL_S = 'S'
 MODEL_3 = '3'
 MODEL_X = 'X'
@@ -24,6 +24,16 @@ ATV = 'A'
 ROADSTER_V2 = 'R'
 SEMI_TRUCK = 'S'
 ALL_TESLAS = 'S3XY CARS'
+
+SURYA = 'SURYA'  # TODO once model name is public
+
+RT1 = 'RT1' 
+RS1 = 'RS1'
+
+F150_LIGHTNING = 'F-150L'
+MACH_E = 'MACH-E'
+
+EV6 = 'EV6'
 
 # Vehicle color CONTSTANTS - ROY G BIV + others
 RED = 0
@@ -37,17 +47,22 @@ VIOLET = 6
 # Moving hardware CONTSTANTS
 GAS_PEDAL = 0                           # Pedal closest to center of the car
 GAS_PEDAL_POSITION = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-DEFAULT_MAX_GAS_PEDAL_TRAVEL = 420      # Units are in millimeters
-MAX_GAS_PEDAL_VALUE = 0x69              # Units are TODO
-BRAKE_PEDAL = 1                         # 2nd pedal from center of thr car
-DEFAULT_MAX_BRAKE_PEDAL_TRAVEL = 69     # Units are in millimeters
-MAX_BRAKE_PEDAL_VALUE = 0xFF            # Units are TODO
-MIN_CAN_BUS_TIMESTEP = 0.001            # Units are seconds
+MAX_GAS_PEDAL_TRAVEL = TOD)     # Units are in millimeters
+
+BRAKE_PEDAL = 1                 # 2nd pedal from center of the car
+BRAKE_PEDAL_POSITION = [TODO, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+MAX_BRAKE_PEDAL_TRAVEL = 69     # Units are in millimeters
+
+MIN_CAN_BUS_TIMESTEP = 0.001    # Units are Seconds
     
 # User Interface CONSTANTS
-UI_TERMINAL_DELAY = 0.1                 # Units are seconds
-MAX_UI_DEALY = 2.0                      # Units are seconds
-FUNCTION_DELAY = 5.0                    # Units are milliSeconds
+UI_TERMINAL_DELAY = 0.1         # Units are seconds
+MAX_UI_DEALY = 2.0              # Units are seconds
+FUNCTION_DELAY = 5.0            # Units are milliseconds
+
+# Integer unit CONSTANTS
+PERCENTAGE_UNITS = '%'
+MILLIMETER_UNITS = 'mm'
 
 
 class GlobalConstant:
@@ -55,10 +70,8 @@ class GlobalConstant:
     def unitTest():
         assert UI_TERMINAL_DELAY < 0.5
         
-        assert MAX_BRAKE_PEDAL_VALUE == 0xFF
-        assert MAX_BRAKE_PEDAL_VALUE < 256
-        assert MAX_BRAKE_PEDAL_VALUE == 255
-        assert not MAX_BRAKE_PEDAL_VALUE < 254
+        assert MAX_GAS_PEDAL_TRAVEL == 0xFF
+        assert MAX_BRAKE_PEDAL_TRAVEL < 256
 
         command = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         assert command == GAS_PEDAL_POSITION
@@ -74,5 +87,5 @@ class GlobalConstant:
 
 
 if __name__ == "__main__":
-    print("Starting GlobalConstant.py unitTest() for TesMuffler App")
+    print("Starting GlobalConstant.py unitTest() for TesMuffler library")
     GlobalConstant.unitTest()
--- TesMuffler/TeslaCanBus.py
+++ TesMuffler/TeslaCanBus.py
@@ -11,7 +11,8 @@ __doc__     = "Read only class for the OBD-II Tesla CAN Bus"
 # https://python-can.readthedocs.io/en/2.1.0/index.html#
 # https://teslatap.com/modifications/extracting-internal-vehicle-data/
 # https://www.obdlink.com/products/obdlink-mxp/
-import can
+import can as canBus
+#import can-isotp as canBus https://github.com/pylessard/python-can-isotp
 
 # Allow program to extract filename of the current file
 # https://docs.python.org/3/library/os.html
@@ -45,8 +46,9 @@ class TeslaCanBus:
     # Debugging CONSTANTS
     DEBUG_STATEMENTS_ON = True
 
-    FAST = 1000000  # 1 Mbit/sec (Mbps)
-    SLOW = 20000    # 20 kbits/sec (kbps)
+    FAST = 1_000_000  # 1 Mbit/sec (Mbps)
+    DEFAULT = 50_000  # 500 kbits/sec (kbps)
+    SLOW = 20_000     # 20 kbits/sec (kbps)
 
     def unitTest():
         calibrationObject = TeslaCalibration(GC.MODEL_S)
@@ -66,7 +68,7 @@ class TeslaCanBus:
         model_3_Default = TeslaCanBus()
         assert TeslaCanBus.readGasPedalPosition(), "Default software initalization of Model S CAN bus, failed on Model 3 hardware"   # NOQA: E501                                 # THIS SHOULD FAIL 3 != S
         
-        model_3_Fast1 = TeslaCanBus(1, GC.MODEL_3)
+        model_3_Fast1 = TeslaCanBus(1, GC.MODEL_3, TeslaCanBus.FAST)
         #TODO
         
         model_3_Slow1 = TeslaCanBus(2, GC.MODEL_3, TeslaCanBus.SLOW)
@@ -75,25 +77,24 @@ class TeslaCanBus:
         model_3_Slow0 = TeslaCanBus(0, GC.MODEL_3, TeslaCanBus.SLOW)
         # TODO 
     
-        model_Y_Default = TeslaCanBus(0, GS.MODEL_Y, TeslaCanBus.FAST, calibrationObject)
+        model_Y_Default = TeslaCanBus(0, GS.MODEL_Y, TeslaCanBus.DEFAULT, calibrationObject)
         # TODO
         
         # TODO model_y, cyberTruck, ATV, Roadster (IN THAT ORDER)
 
 
-    def __init__(self, year=2019, carModel=GC.MODEL_S, channel=0, bitrate=FAST,
-                 calibrationObject=GC.DEFAULT_MAX_GAS_PEDAL_TRAVEL):
+    def __init__(self, year=2019, carModel=GC.MODEL_S, channel=0, bitrate=DEFAULT, calibrationObject=GC.DEFAULT_MAX_GAS_PEDAL_TRAVEL):
 
         thisCodesFilename = os.path.basename(__file__)
         self.DebugObject = Debug(TeslaCanBus.DEBUG_STATEMENTS_ON, thisCodesFilename)
 
         self.CalibratedMaxGasPedalTravel = calibrationObject.gasPedalMax()
         self.CalibratedMaxBrakePedalTravel = calibrationObject.brakePedalMax()
-
+        
         if(carModel == GC.MODEL_S and year >= 2020):  #TODO Check years supported
-            self.bus = can.interface.Bus(bustype=TODO)
+            self.bus = canBus.interface.Bus(bustype=TODO)
         elif(carModel == GC.MODEL_3 and year >= 2019):
-            self.bus = can.interface.Bus(bustype=TODO)
+            self.bus = canBus.interface.Bus(bustype=TODO)
         elif(carModel == GC.MODEL_X and year >= 2020):  #TODO Check years supported
             self.bus = can.interface.Bus(bustype=TODO)
         elif(carModel == GC.MODEL_Y and year >= 2020):  #TODO Check years supported
@@ -105,68 +106,69 @@ class TeslaCanBus:
         elif(carModel == GC.ROADSTER_V2 and year >= 2022):  #TODO Check years supported
             self.bus = can.interface.Bus(bustype=TODO)
         elif(carModel == GC.SEMI_TRUCK and year >= 2023):  #TODO Check years supported
-            self.bus = can.interface.Bus(bustype=TODO)
+            self.bus = canBus.interface.Bus(bustype=TODO)
         else:
             Debug.Lprint("ERROR: This EV's CAN bus is not support by this software. Please buy a Tesla :)")
 
-    def readGasPedalPosition(units):
-        data = TODO
-        msg = can.Message(arbitration_id=0x7df, data=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], extended_id=False)
-        sendMessage(msg)
 
-    def readGasPedalPosition(self, units):
+    def readGasPedalPosition(self, units=GC.PERCENTAGE_UNITS):
+    """
+    Get the current READ-ONLY position of the accelerator/gas pedal  
+    
+    Decoder ID's and TODO in Perl
+    https://github.com/openvehicles/CAN-RE-Tool/blob/master/rules/teslamodels
+    
+    Args:
+        units: (A string CONSTANT from GlobalConstant.py) Defaults to percentage 
+        
+    Returns:
+        Percentage from 0% to 100% inclusivly OR Distance in mm from 0 to MAX_GAS_PEDAL_TRAVEL 
+    """
+
+
         # Command to request from CAN bus the current pedal position
         command = TODO [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
         
-        return canBusPedalSubroutine(GAS_PEDAL, command, units)
+        return canBusPedalSubroutine(GC.GAS_PEDAL, command, units)
 
-    def readGasPedalVelocity(self):
+    def readGasPedalVelocity(self, units=GC.PERCENTAGE_UNITS):
         
-        position1 = readGasPedalPosition(GAS_PEDAL, GC.GAS_PEDAL_POSITION, GC.MILLIMETERS)
+        position1 = readGasPedalPosition(units)
         time.sleep(GC.MIN_CAN_BUS_TIMESTEP)
-        position2 = readGasPedalPosition(GAS_PEDAL, GC.GAS_PEDAL_POSITION, GC.MILLIMETERS)
+        position2 = readGasPedalPosition(units)
     
-        velocity = abs(position2 - position1)/GC.MIN_CAN_BUS_TIMESTEP
-        # Command to request from CAN bus the current pedal position
-        command = TODO
-        
-        data = canBusPedalSubroutine(GAS_PEDAL, command, GC.MILLIMETERS)
-        
-        velocity = data * TODO
-        
-        return velocity
+        return (position2 - position1)/GC.MIN_CAN_BUS_TIMESTEP
 
-    def readGasPedalVAcceleration():
-        # command to request CAN bus to return pedal position
-        command = TODO
+
+    def readGasPedalVAcceleration(self, units):
         
-        data = canBusPedalSubroutine(GAS_PEDAL, command, units)
+        velocity1 = readGasPedalVelocity(units)
+        time.sleep(GC.MIN_CAN_BUS_TIMESTEP)
+        velocity2 = readGasPedalVelocity(units)
         
-        mmPerSecPerSec = data * TODO
-        
-        return mmPerSecPerSec
+        return (velocity2 - velocity1)/GC.MIN_CAN_BUS_TIMESTEP        
 
     
-    def canBusPedalSubroutine(pedalType, command=GC.GAS_PEDAL_POSITION, units=GC.PERCENTAGE_UNITS):
-        msg = can.Message(arbitration_id=0x7df, command=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], extended_id=False)
+    def canBusPedalSubroutine(pedalType=GC.GAS_PEDAL, command=GC.GAS_PEDAL_POSITION, units=GC.PERCENTAGE_UNITS):
+        msg = canBus.Message(arbitration_id=0x7df, command=[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], extended_id=False)
         rawValue = sendMessage(msg)
 
         if(pedalType == GAS_PEDAL):
-            percentOfMax = rawValue/GC.MAX_GAS_PEDAL_VALUE
-            if(units == GS.PERCENTAGE):
+            percentOfMax = rawValue/GC.MAX_GAS_PEDAL_TRAVEL
+            if(units == GC.PERCENTAGE_UNITS):
                 value = percentOfMax
-            elif(units == GS.MILLIMETERS):
+            elif(units == GC.MILLIMETER_UNITS):
                 value = percentOfMax * self.CalibratedMaxGasPedalTravel       
-            elif(units == GS.CENTIMETERS):
+            elif(units == GC.CENTIMETER_UNITS):
                 value = percentOfMax * (self.CalibratedMaxGasPedalTravel/10)
                 
         elif(pedalType == BRAKE_PEDAL):
-            percentOfMax = rawValue/GC.MAX_BRAKE_PEDAL_VALUE
-            if(units == GS.PERCENTAGE):
+            percentOfMax = rawValue/GC.MAX_BRAKE_PEDAL_TRAVEL
+            if(units == GC.PERCENTAGE_UNITS):
                 value = percentOfMax
-            elif(units == GS.MILLIMETERS):
+            elif(units == GC.MILLIMETER_UNITS):
                 value = percentOfMax * self.CalibratedMaxBrakePedalTravel       
-            elif(units == GS.CENTIMETERS):
+            elif(units == GC.CENTIMETER_UNITS):
                 value = percentOfMax * (self.CalibratedMaxBrakePedalTravel/10)
         
         else:
