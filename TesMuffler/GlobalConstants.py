d8e8af22211e151f60c74afc733e38ef7c760c89
Blaze Sanders <blaze.sanders@spoke-aiot.com>
2022-10-02 03:56:47 -0700

    Add colors, cm, gear & rpm CONSTANTS

--- TesMuffler/GlobalConstants.py
+++ TesMuffler/GlobalConstants.py
@@ -3,9 +3,12 @@
 __author__  = "Blaze Sanders"
 __email__   = "dev@blazesanders.com"
 __status__  = "Development"
-__date__    = "Late Updated: 2022-09-28"
-__doc__     = "Useful global constants used across entire TesMuffler library"
+__date__    = "Late Updated: 2022-10-01"
+__doc__     = "Useful global constants for the entire TesCustoms TesMuffler library"
 """
+TODO = -1
+
+DEBUG_STATEMENTS_ON = True
 
 # Vehicle make name CONTSTANTS - Yes these are sorted best to worst :)
 TESLA = 0
@@ -43,14 +46,18 @@ GREEN = 3
 BLUE = 4
 INDIGO = 5
 VIOLET = 6
+GREY = 7
+WHITE = 8
 
 # Moving hardware CONTSTANTS
-GO_PEDAL = 0                   # Pedal furthest right
-GO_PEDAL_POSITION = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-MAX_GO_PEDAL_TRAVEL = TODO     # Units are in millimeters
+GO_PEDAL = 0                    # Pedal furthest right
+GO_PEDAL_POSITION_ID = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+MAX_GO_PEDAL_TRAVEL = TODO      # Units are in millimeters
+TOP_GEAR = 5
+MAX_RPM = 10_000
 
 BRAKE_PEDAL = 1                 # 2nd pedal from center of the car
-BRAKE_PEDAL_POSITION_ID = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  #TODO
+BRAKE_PEDAL_POSITION_ID = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]  # TODO
 MAX_BRAKE_PEDAL_TRAVEL = 69     # Units are in millimeters
 
 # User Interface CONSTANTS
@@ -59,32 +66,32 @@ MAX_UI_DEALY = 2.0              # Units are seconds
 FUNCTION_DELAY = 0.005          # Units are seconds
 MIN_CAN_BUS_TIMESTEP = 0.001    # Units are seconds
 
-# Integer unit CONSTANTS
+# Dimensional unit CONSTANTS
 PERCENTAGE_UNITS = '%'
 MILLIMETER_UNITS = 'mm'
+CENTIMETER_UNITS = 'cm'
 
 
-class GlobalConstant:
+class GlobalConstants:
 
     def unitTest():
         assert UI_TERMINAL_DELAY < 0.5
-        
-        assert MAX_GO_PEDAL_TRAVEL == 0xFF
+
+        assert MAX_GO_PEDAL_TRAVEL == TODO
         assert MAX_BRAKE_PEDAL_TRAVEL < 256
 
         command = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        assert command == GO_PEDAL_POSITION
+        assert command == GO_PEDAL_POSITION_ID
 
         assert RED == 0
-        
+
         assert MODEL_S == 'S'
-        assert MODEL_S == "S" 
+        assert MODEL_S == "S"
 
         assert TESLA != FORD
-       
+
         assert MODEL_S == '3', "Unit Test failed successfully"
-
-
+    
 if __name__ == "__main__":
-    print("Starting GlobalConstant.py unitTest() for TesMuffler library")
-    GlobalConstant.unitTest()
+    print("Starting GlobalConstants.py unitTest() for TesMuffler library")
+    GlobalConstants.unitTest()
