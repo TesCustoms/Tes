30d840f4fef300534114f258c7e768306459e70d
Blaze Sanders <blaze.sanders@spoke-aiot.com>
2022-10-02 03:53:41 -0700

    Removed firebase to add supabase database imports and env variables, and started digital enggine and car Objects

--- TesMuffler/TesMufflerDriver.py
+++ TesMuffler/TesMufflerDriver.py
@@ -3,7 +3,7 @@
 __author__  = "Blaze Sanders"
 __email__   = "dev@blazesanders.com"
 __status__  = "Development"
-__date__    = "Late Updated: 2022-09-30"
+__date__    = "Late Updated: 2022-10-01"
 __doc__     = "TesMuffler project code starts running here"
 """
 
@@ -11,29 +11,30 @@ __doc__     = "TesMuffler project code starts running here"
 # https://docs.python.org/3/library/argparse.html
 import argparse
 
-# Allow program use enviroment variables and determine filename of current file
+# Allow program to exit safety and use enviroment variables for passwords and API keys
 # https://docs.python.org/3/library/os.html
 import os
-
 import sys
 
-# Allow data from iOS and Android Supabase database to be accessed via API
-# https://docs.python.org/3/library/json.html
-import json
-
-# Open source NoSQL databased
+# Open source plaform for NoSQL databases, authentication, file storage, and auto-generated APIs
 # https://github.com/supabase-community/supabase-py
 from supabase import create_client
 
-# Play custom engine sounds
-# http://www.mega-nerd.com/SRC/
-from EngineSoundGenerator import *
+try:  # importing internally developed libraries
+    # Create pitch varying audio of a library of cars in real-time on low processing power CPUs
+    from EngineSoundGenerator import *
 
-# Global constants of TesCustoms TesMuffler library
-import GlobalConstants as GC
+    # Realtime description of a Car objects state including RPM, gear, make, model, year, and color
+    from Car import *
 
+    # Useful global constants for the entire TesCustoms TesMuffler library
+    import GlobalConstants as GC
 
-# Flexible event logging system for DEBUGGING, ERRORS, and INFO 
+except ImportError:  #TODO
+    print("EngineSoundGenerator.py, car.py, OR  GlobalConstants.py didn't import correctly")
+    print("Please verify that those files are in same directory as the TesMufflerDriver.py")
+
+# Flexible event logging system for DEBUGGING, ERRORS, and INFO
 # https://docs.python.org/3/library/logging.html
 import logging
 
@@ -41,36 +42,47 @@ import logging
 if __name__ == "__main__":
 
     # Create Loggers for the 4 major subsystsems
-    CanBusLog = logging.Logger("CanBus.log")
-    WirelessLog = logging.Logger("Wireless.log")
-    EngineSoundLog = logging.Logger("EngineSound.log")
-    QRCodeLog = logging.Logger("QRCode.log")
+    CanBusLog = logging.getLogger("CanBus.log")
+    WirelessLog = logging.getLogger("Wireless.log")
+    EngineSoundLog = logging.getLogger("EngineSound.log")
+    QRCodeLog = logging.getLogger("QRCode.log")
 
     if(GC.DEBUG_STATEMENTS_ON):
-        print("Debugging print statments are on for CanBus, Bluetooth, EngineeSound, and QR Code Loggers")
-        CanBusLog.setLevel(logging.DEBUG)
-        WirelessLog.setLevel(logging.DEBUG)
-        EngineSoundLog.setLevel(logging.DEBUG)
-        QRCodeLog.setLevel(logging.DEBUG)
+        print("Debugging print statments are on for ALL Loggers")
+        logging.basicConfig(level=logging.DEBUG)
 
     else:
-        print("Custom user info print statement Loggers have been configured for PRODUCTION code")
+        print("IN PRODUCTION CODE MODE: Custom print statement Loggers have been configured")
+        #TODO NON-BASIC CONFIG
         CanBusLog.setLevel(logging.INFO)
         WirelessLog.setLevel(logging.INFO)
         EngineSoundLog.setLevel(logging.ERROR)
         QRCodeLog.setLevel(logging.CRITICAL)
 
-    TES_API_URL = os.environ.get('TES_API_URL')
-    TES_API_KEY = os.environ.get('TES_API_KEY')
-    supabase = create_client(TES_API_URL, TES_API_KEY)
+    API_URL = os.environ.get('TESMUFFLER_SUPABASE_API_URL')
+    API_KEY = os.environ.get('TESMUFFLER_SUPABASE_API_KEY')
+    supabase = create_client(API_URL, API_KEY)
 
-    digitalEngine = EngineSoundGenerator(EngineSoundGenerator.MC_LAREN_F1, EngineSoundLog)
+    digitalEngine = EngineSoundGenerator(EngineSoundGenerator.MC_LAREN_F1)  # NOQA F405
+
+    carMake = GC.TESLA       #TODO = supabase.__getattribute__(make)
+    carModel = GC.MODEL_3    #TODO = supabase.__getattribute__(model)
+    carYear = 2022           #TODO = supabase.__getattribute__(year)
+    carColor = GC.WHITE      #TODO = supabase.__getattribute__(color)
+    digitalCar = Car(carMake, carModel, carYear, carColor)
 
     while(True):
-
+        #TODO if supabase.__getattribute__(changeBit)
+        
         try:
+            digitalCar.update()
             digitalEngine.startAudioLoop()
 
         except KeyboardInterrupt:
             print(f"\nEXITTING PROGRAM")
             sys.exit(0)
+
+        # TODO
+        #except ProgramCrash:
+        #    print(f"\nEXITTING PROGRAM")
+        #    check_call("python3 TesMufflerDriver.py", shell=True)
